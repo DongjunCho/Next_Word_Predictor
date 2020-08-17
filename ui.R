44f257c12e613763594ddb39af253525ea38fafb
DongjunCho <67394778+DongjunCho@users.noreply.github.com>
2020-08-17 14:01:09 -0700

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,43 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinythemes)
+library(stringr)
+library(markdown)
+library(dplyr)
+library(tm)
+library(stylo)
+
+shinyUI(
+    navbarPage("Next Word Predictor",
+               theme = shinytheme("spacelab"),
+               tabPanel("",
+                        fluidPage(
+                            titlePanel("Home"),
+                            sidebarLayout(
+                                sidebarPanel(
+                                    textInput("userInput",
+                                              "Enter a word or phrase:",
+                                              value =  "",
+                                              placeholder = "Enter text here")
+                                ),
+                                mainPanel(
+                                    h4("Input text"),
+                                    verbatimTextOutput("userSentence"),
+                                    br(),
+                                    h4("Predicted words"),
+                                    verbatimTextOutput("prediction")
+                                )
+                            )
+                        )
+               )
+    )
+)
+
